96be3eec73f5038c83507ee715bb1422fd2be0be
yistc <32131698+yistc@users.noreply.github.com>
2022-06-08 23:22:55 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,7 @@
 #!/bin/sh
-sudo apt -y install zsh unzip curl vim git ripgrep lsof
 
-cat >>$HOME/.zshrc<<EOF
-alias ssh-ls="rg '^Host ([^*]+)$' ~/.ssh/config"
-alias git-all="gaa && gcam '1' && gp"
-alias surge-cli='/Applications/Surge.app/Contents/Applications/surge-cli'
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-alias lsl="ls -lF"
-alias lsal="ls -alF"
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias path='echo -e ${PATH//:/\\n}'
-EOF
+# tools
+sudo apt -y install zsh unzip curl vim git ripgrep lsof
 
 # vim config
 wget https://gist.githubusercontent.com/yistc/29363df11d7b67d7b79850c6b39942b0/raw/vimrc
